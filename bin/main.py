613a592d6e98b8ea2aa0750c019db0ab3c540ab7
Mohammad Rimawi <mohrimawiz@gmail.com>
2023-06-16 13:23:06 +0300

    Commiting to dev branch

--- bin/main.py
+++ bin/main.py
@@ -1,10 +1,15 @@
+from enum import Enum, auto
+import json
 from os import getenv
+from typing import Optional
+from uuid import UUID, uuid4
 
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from Models.Cores import NotificationMessage, Socket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Body, Response, status
+from Models.Cores import NotificationMessage, Socket, Connection, Channel, Verbosity
 from services import Notifier
+from pydantic import BaseModel, Field
 
 load_dotenv()
 PORT = int(getenv("NOTIFICATION_CORE_PORT"))
@@ -14,36 +19,146 @@ app = FastAPI(title="Varla-Notification-Core")
 notifier = Notifier()
 
 
-@app.websocket("/bind/{channel_name}")
-async def websocket_endpoint(websocket: WebSocket, channel_name: str):
-    socket = Socket(websocket=websocket, acknowledged=True)
-    await notifier.subscribe(channel_name=channel_name, socket=socket)
-    print(notifier.connections)
-    try:
-        await socket.websocket.send_text(f"Connected!")
-        while True:
-            data = await socket.websocket.receive()
-            print("socket", data)
-            if data["type"] == "websocket.disconnect":
-                print(socket.websocket, "bye")
-                notifier.remove(channel_name=channel_name, socket=socket.websocket)
-                return
-            elif data["text"] == "alive":
-                socket.acknowledged = True
-            else:
-                await websocket.send_text(f"Message text was: {data}")
-    except WebSocketDisconnect:
-        notifier.remove(channel_name=channel_name, websocket=socket.websocket)
+# @app.websocket("/bind/{channel_name}")
+# async def websocket_endpoint(websocket: WebSocket, channel_name: str):
+#     socket = Socket(websocket=websocket, acknowledged=True)
+#     print(Connection(websocket=socket))
+#     print(uuid4())
+#     # await notifier.subscribe(channel_name=channel_name, socket=socket)
+#     print(notifier.connections)
+#     try:
+#         await socket.websocket.send_text(f"Connected!")
+#         while True:
+#             data = await socket.websocket.receive()
+#             print("socket", data)
+#             if data["type"] == "websocket.disconnect":
+#                 print(socket.websocket, "bye")
+#                 notifier.remove(channel_name=channel_name, socket=socket.websocket)
+#                 return
+#             elif data["text"] == "alive":
+#                 socket.acknowledged = True
+#             else:
+#                 await websocket.send_text(f"Message text was: {data}")
+#     except WebSocketDisconnect:
+#         notifier.remove(channel_name=channel_name, websocket=socket.websocket)
+
+
+class subsc(BaseModel):
+    connection_id: UUID
+    channels: list[Channel]
 
 
 @app.get("/push/{channel_name}/{message}")
 async def push_to_connected_websockets(channel_name: str, message: str):
-    await notifier.push(NotificationMessage(message=message, channel_name=channel_name))
+    await notifier.push(
+        NotificationMessage(message=message, channel_names=[channel_name])
+    )
 
 
 @app.post("/push")
 async def push_post(message: NotificationMessage):
     await notifier.push(message)
+
+
+async def bind(connection: Connection):
+    await connection.websocket.send_text(
+        json.dumps(
+            {
+                "connection_id": str(connection.id),
+                "text": f"Connected, your connection id: {str(connection.id)} ",
+            }
+        )
+    )
+
+    while True:
+        data = await connection.websocket.receive()
+        print("socket", data)
+        if data["type"] == "websocket.disconnect":
+            print(connection, "bye")
+            notifier.pop(connection_id=connection.id)
+            return
+        if data["text"] == "alive":
+            connection.acknowledged = True
+        else:
+            await connection.websocket.send_text(f"Message text was: {data}")
+
+
+@app.websocket("/bind/{channel_name}")
+@app.websocket("/connect")
+async def connect(websocket: WebSocket, connection_id: Optional[UUID] = None):
+    await websocket.accept()
+    connection = Connection(websocket=websocket, id=connection_id)
+    notifier.connections[connection.id] = connection
+
+    # await subscribe(
+    #     channels=[Channel(name="string", verbosity=Verbosity.NORMAL)],
+    #     connection_id=connection.id,
+    # )
+    print(notifier.connections)
+    await bind(connection)
+
+
+@app.websocket("/reconnect/{connection_id}")
+async def reconnect(websocket: WebSocket, connection_id: UUID, response: Response):
+
+    if connection_id in notifier.connections:
+
+        await websocket.accept()
+
+        notifier.connections[connection_id].websocket = websocket
+
+        print(notifier.connections)
+        await bind(notifier.connections[connection_id])
+    else:
+        # response.status_code = status.ws
+        # await websocket.close(code=status.WS_1001_GOING_AWAY, reason="Invalid ID")
+
+        # return "Unknown connect_id!"
+
+        return await connect(websocket=websocket, connection_id=connection_id)
+
+
+@app.post("/disconnect")
+async def disconnect(connection_id: UUID = Body(embed=True)):
+    await notifier.disconnect(connection_id)
+    pass
+
+
+@app.post("/close")
+async def close(connection_id: UUID = Body()):
+    await notifier.close(connection_id)
+    pass
+
+
+@app.post("/subscribe")
+async def subscribe(channels: list[Channel], connection_id: UUID = Body()):
+    try:
+        await notifier.subscribe(channels, connection_id)
+        return "Subscribed!"
+    except KeyError:
+        return "Unknown connect_id!"
+
+
+@app.post("/list_subscribtions")
+async def list_subscribtions(connection_id: UUID = Body(embed=True)):
+    try:
+        return notifier.connections[connection_id].channels
+
+    except KeyError:
+        return "Unknown connect_id!"
+
+
+@app.post("/unsubscribe")
+async def unsubscribe(channel_names: list[str] = Body(), connection_id: UUID = Body()):
+    try:
+        await notifier.unsubscribe(
+            channel_names=channel_names, connection_id=connection_id
+        )
+        return "Subscribed!"
+    except KeyError:
+        return "Unknown connect_id!"
+    except Exception as e:
+        print(e)
 
 
 @app.on_event("startup")
@@ -52,4 +167,4 @@ async def startup():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=PORT, host="0.0.0.0")
+    uvicorn.run("main:app", port=PORT, host="0.0.0.0", reload=True)
--- bin/services/Notifier/main.py
+++ bin/services/Notifier/main.py
@@ -1,66 +1,127 @@
 # From https://gist.github.com/francbartoli/2532f8bd8249a4cefa32f9c17c886a4b
 # From https://github.com/tiangolo/fastapi/issues/258
 
+from enum import Enum, auto
+import json
 from typing import List
+from uuid import UUID
+from starlette.websockets import WebSocket
 
-from Models.Cores import NotificationMessage, Socket
+from Models.Cores import NotificationMessage, Socket, Connection, Channel, Verbosity
 
 
 class Notifier:
     def __init__(self):
-        self.connections: dict[str, List[Socket]] = {}
+
+        self.connections: dict[UUID, List[Connection]] = {}
+        self.channels: dict[str, List[UUID]] = {}
+
         self.generator = self.get_notification_generator()
 
     async def get_notification_generator(self):
         while True:
-            message, channel_name = yield
-            await self._notify(channel_name=channel_name, message=message)
+            message = yield
+            await self._notify(message=message)
+
+    async def _notify(self, message: NotificationMessage):
+        anti_spam_list = []
+        print("HIIIIIIIIIIIIIIIIII")
+        for channel_name in message.channel_names:
+            if channel_name not in self.channels:
+                continue
+            for connection_id in self.channels[channel_name]:
+                try:
+                    if (
+                        connection_id not in anti_spam_list
+                        and self.connections[connection_id].websocket
+                    ):
+                        await self.connections[connection_id].websocket.send_text(
+                            json.dumps({"text": message.message, "from": channel_name})
+                        )
+                        anti_spam_list.append(connection_id)
+                except Exception as e:
+                    print("broken", self.connections[connection_id].websocket, e)
+                    await self.disconnect(connection_id=connection_id)
+
+    async def send(self, connection_id: UUID, message: dict):
+        await self.connections[connection_id].websocket.send_text(json.dumps(message))
 
     async def push(self, message: NotificationMessage):
-        await self.generator.asend((message.message, message.channel_name))
-        print(self.connections)
+        await self.generator.asend((message))
 
-    async def subscribe(self, channel_name: str, socket: Socket):
-        await socket.websocket.accept()
+    async def subscribe(self, channels: list[Channel], connection_id: UUID):
+        for channel in channels:
+            if channel.name not in self.channels:
+                self.channels[channel.name] = []
 
-        if channel_name not in self.connections:
-            self.connections[channel_name] = []
+            self.channels[channel.name].append(connection_id)
+            self.connections[connection_id].channels.append(channel)
 
-        self.connections[channel_name].append(socket)
+            await self.send(
+                connection_id,
+                message={
+                    "from": channel.name,
+                    "text": f"Subscribed!",
+                },
+            )
 
-    def remove(self, channel_name: str, socket: Socket):
-        print(self.connections)
+    async def unsubscribe(self, channel_names: list[str], connection_id: UUID):
+
+        for channel_name in channel_names:
+
+            if (
+                channel_name in self.channels
+                and connection_id in self.channels[channel_name]
+            ):
+
+                self.channels[channel_name].remove(connection_id)
+
+                self.connections[connection_id].channels = [
+                    channel
+                    for channel in self.connections[connection_id].channels
+                    if channel.name != channel_name
+                ]
+
+                await self.send(
+                    connection_id,
+                    message={
+                        "from": channel_name,
+                        "text": f"Unsubscribed!",
+                    },
+                )
+
+    def pop(self, connection_id: UUID) -> WebSocket:
+        socket: WebSocket = self.connections[connection_id].websocket
+        self.connections[connection_id].websocket = None
+        return socket
+
+    async def disconnect(self, connection_id: UUID):
         try:
-            socket.acknowledged = True
-            self.connections[channel_name].remove(socket)
-        except:
+            socket: WebSocket = self.pop(connection_id)
+
+            if socket:
+                await socket.close()
+
             print(self.connections)
-            print("ya zeft")
 
-    async def close_all(self):
-        for channel in self.connections:
-            while len(self.connections[channel]) > 0:
-                socket = self.connections[channel].pop()
-                await self.close(socket)
+        except Exception as e:
+            print(e)
+            print(self.connections)
 
-    async def close(self, socket: Socket):
-        await socket.websocket.close()
+    async def close(self, connection_id: UUID):
+        try:
+            self.connections.pop(connection_id)
 
-    async def _notify(self, channel_name: str, message: str):
-        living_connections = []
-        while (
-            channel_name in self.connections and len(self.connections[channel_name]) > 0
-        ):
-            socket = self.connections[channel_name].pop()
+        except Exception as e:
+            print(e)
+            print(self.connections)
+            # raise e
 
-            # if socket.acknowledged:
-            socket.acknowledged = False
-            try:
-                living_connections.append(socket)
-                await socket.websocket.send_text(message)
-            except:
-                print("broken", socket)
-                self.remove(channel_name, socket)
-            # else: await self.close(socket)
+    # async def close_all(self):
+    #     for channel in self.connections:
+    #         while len(self.connections[channel]) > 0:
+    #             socket = self.connections[channel].pop()
+    #             await self.close(socket)
 
-        self.connections[channel_name] = living_connections
+    # async def close(self, socket: Socket):
+    #     await socket.websocket.close()
